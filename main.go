f29455866625b524d02487cefff92006f1fe3eb4
Nikhil Bardeja <bardejanikhil@gmail.com>
2026-07-05 00:40:24 +0530

    done print and variables

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var name string = "Raju"
+	// var age int = 5
+	// var year = 2026
+	// var fl float64 = 64.35
+	// var valid bool = true
+	// const pie = 3.14
+
+	kuchbhi := 987
+	fmt.Print(kuchbhi)
+
+}
+
+// IF a variable is initialized by CAPITAL letter then it is PUBLIC
+// if by SMALL letter it is PRIVATE (accessible only at is package)
---
+++ myutil/myutil.go
@@ -0,0 +1,7 @@
+package myutil
+
+import "fmt"
+
+func P(m string) {
+	fmt.Println(m)
+}
