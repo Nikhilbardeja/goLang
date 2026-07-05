bf6f76254f7bc3a1cdd9f17895ef628b8e28bc10
Nikhil Bardeja <bardejanikhil@gmail.com>
2026-07-05 23:01:26 +0530

    functions

---
+++ functions/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(add(8, 5))
+}
+
+func add(a int, b int) int {
+	return a + b
+}
+
+// in param list if you add data type at the end
+// all the param will be declared with same datatype
+func add1(a, b int) int {
+	return a + b
+}
+
+// need to define datatype of each return value
+func add2(a, b int) (int, int) {
+	return a + b, a - b
+}
+
+// result variable is already defined at return datatype definition
+// and just a single return statment will automatically return the result variable
+func add3(a, b int) (result int) {
+	result = a + b
+	return
+}
